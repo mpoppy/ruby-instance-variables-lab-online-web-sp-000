14fd45df91a9a3835c653e337e13b0e7c4c16691
Marc Poppy <poppy.marc@gmail.com>
2019-05-30 04:45:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 
 class Dog
   def name=(dog_name) #this method takes the argument and sets equal to a variable
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   def name #this method is for reading the name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
